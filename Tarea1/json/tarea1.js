589f3183046da52b7aa3cd7892ad9520510d50a9
DavidDalmauDieguez <david.dieguez13@gmail.com>
2022-05-31 08:55:52 +0200

    Ejercicio 1

---
+++ Tarea1/json/tarea1.js
@@ -0,0 +1,10 @@
+// Se muestran mensajes por consola.
+console.log('Hola Mundo!');
+console.log('Soy el primer script');
+// Se obtiene el párrafo por ID.
+let campoParrafo = document.getElementsByClassName('parrafo');
+console.log(campoParrafo);
+// Se setea un valor al párrafo.
+campoParrafo[0].innerHTML = "Soy el primer párrafo";
+// Se muestra una alerta.
+alert('Por favor active el soporte para javascript');
